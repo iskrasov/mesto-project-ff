505b7f2a4ec2fe5ffc5b6198cb1fd5b87c1896b8
Stanislav.Krasovskiy <iskrasov@yandex.ru>
2024-12-05 15:45:45 +0300

    Внес правки после 1 итерации правок ревьюера

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,26 @@
-
-
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector('#card-template');
-
-// Компоненты, из которых собираю карточку
-const cardImage = cardTemplate.querySelector('.card__image');
-const cardDelBtn = cardTemplate.querySelector('.card__delete-button');
-const cardTitle = cardTemplate.querySelector('.card__title');
-const cardLikeBtn = cardTemplate.querySelector('.card__like-button');
 
 
 // @todo: DOM узлы
 // Нахожу и объявляю здесь контейнер, в котором содержится основной контент; в контейнере уже, в свою очередь, контейнер-список для карточек
 const container = document.querySelector('.content');
-const cardList = container.querySelector('.places__list');
+const cardsContainer = container.querySelector('.places__list');
 
 
 // @todo: Функция создания карточки
 // С помощью этой ф-ции обращаюсь к шаблону карточки, клонирую его, собираю карточку из комопнентов, добавляю карточку в конец списка карточек.
-function addCard({name,link}, delCard) {
-  const cardList = container.querySelector('.places__list');
+function createCard({name,link}, delCard) {
   const cardTemplate = document.querySelector('#card-template').content;
   const cardItem = cardTemplate.querySelector('.places__item').cloneNode(true);
+  const cardImage = cardItem.querySelector('.card__image');
 
-  cardItem.querySelector('.card__image').src = link;
+  cardImage.src = link;
+  cardImage.alt = name;
   cardItem.querySelector('.card__title').textContent = name;
-  cardItem.querySelector('.card__image').alt = name;
 
   const cardDelBtn = cardItem.querySelector('.card__delete-button');
   cardDelBtn.addEventListener('click', delCard);
-
-  function delCard() {
-    const cardItemDel = cardDelBtn.closest('.places__item');
-    cardItemDel.remove();
-    };
 
   return cardItem;
 };
@@ -41,6 +28,8 @@ function addCard({name,link}, delCard) {
 
 // @todo: Функция удаления карточки
 function delCard() {
+  const cardItem = document.querySelector('.places__item');
+  const cardDelBtn = cardItem.querySelector('.card__delete-button');
   const cardItemDel = cardDelBtn.closest('.places__item');
   cardItemDel.remove();
   };
@@ -49,11 +38,10 @@ function delCard() {
 // @todo: Вывести карточки на страницу
 // Рендерю карточки в контейнер ul .places__list
 function renderCard() {
-  const cardList = container.querySelector('.places__list');
-  initialCards.forEach(({name,link,alt}) => 
+  initialCards.forEach(({name,link}) => 
     {
-    const cardItem = addCard({name,link,alt}, delCard);
-    cardList.append(cardItem);
+    const cardItem = createCard({name,link}, delCard);
+    cardsContainer.append(cardItem);
   });
 };
 
